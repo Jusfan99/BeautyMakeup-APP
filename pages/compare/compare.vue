73938a3882bb02bbf1c84dfd10465dd8dcffcded
xcm1115 <997749670@qq.com>
2020-01-17 15:23:00 +0800

    Initialization

---
+++ pages/compare/compare.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<view class="example-body">
+			<uni-nav-bar color="#F06365" title="比价" shadow="true" statusBar="false"></uni-nav-bar>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniNavBar from '@/components/uni-nav-bar/uni-nav-bar.vue'
+	export default {
+		components: {
+			uniNavBar
+		},
+		data() {
+			
+		},
+		onLoad() {
+	
+		},
+		methods: {
+	
+		}
+	}
+</script>
+
+<style>
+	.example-body {
+		padding: 0;
+	}
+</style>
---
+++ pages/home/home.vue
@@ -0,0 +1,65 @@
+<template>
+	<view>
+		<view class="example-body">
+			<uni-nav-bar color="#F06365" title="首页" shadow="true" statusBar="false"></uni-nav-bar>
+		</view>
+		<view class="content">
+			<image class="logo" src="/static/logo.png"></image>
+			<view class="text-area">
+				<text class="title">{{title}}</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniNavBar from '@/components/uni-nav-bar/uni-nav-bar.vue'
+	export default {
+		components: {
+			uniNavBar
+		},
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.example-body {
+		padding: 0;
+	}
+	
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
